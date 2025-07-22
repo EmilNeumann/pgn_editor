6049dd794c20b34d90ea092095b1ab9bc3109b98
Emil <emil.neumann1@web.de>
2025-07-22 18:21:16 +0200

    changed structure to OOP, added event handlers for replay and practice modes

--- main.py
+++ main.py
@@ -24,26 +24,6 @@ def get_piece_surface(piece_type, color):
     return pygame.image.load(buffer)
 
 
-def get_coordinates(file_index, rank_index, orientation, border):
-    if orientation:
-        rank_index = 7 - rank_index
-    else:
-        file_index = 7 - file_index
-    x = 15*border + 45*file_index
-    y = 15*border + 45*rank_index
-    return x, y
-
-
-def get_square(x, y, orientation, border):
-    file_index = (x - 15*border) // 45
-    rank_index = (y - 15*border) // 45
-    if orientation:
-        rank_index = 7 - rank_index
-    else:
-        file_index = 7 - file_index
-    return file_index, rank_index
-
-
 def get_color_from_nags(nags: set[int]) -> str:
     if not nags:
         return "green"
@@ -64,83 +44,145 @@ def get_color_from_nags(nags: set[int]) -> str:
     return "#ff00ff"
 
 
-def draw(node, orientation):
-    window = pygame.display.get_surface()
-    board = node.board()
-    arrows = []
-    if len(node.variations) > 1:
-        for child_node in node.variations:
-            arrows.append(
-                chess.svg.Arrow(
-                    child_node.move.from_square,
-                    child_node.move.to_square,
-                    color=get_color_from_nags(child_node.nags)
-                )
-            )
-    board_svg = chess.svg.board(
-        board,
-        orientation=orientation,
-        arrows=arrows,
-        size=SIZE
-    )
-    buffer = io.BytesIO(board_svg.encode())
-    board_surface = pygame.image.load(buffer)
-    window.blit(board_surface, (0, 0))
-    for file_index, rank_index in itertools.product(range(8), repeat=2):
+class EventHandler:
+    def __init__(self, parent):
+        self.parent: Window = parent
+    
+    def key_down(self, event):
+        pass
+    
+    def mouse_button_down(self, event):
+        pass
+
+
+class ReplayMode(EventHandler):
+    def key_down(self, event):
+        if event.key == pygame.K_f:
+            self.parent.flip_board()
+        node = self.parent.node
+        if event.key == pygame.K_LEFT:
+            self.parent.node = node.parent or node
+        if event.key == pygame.K_RIGHT:
+            self.parent.make_random_move()
+
+
+class PracticeMode(EventHandler):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.active_square = None
+        self.parent.make_random_move()
+    
+    def key_down(self, event):
+        if event.key == pygame.K_f:
+            self.parent.flip_board()
+        if event.key == pygame.K_r:
+            # reset to root node
+            self.parent.node = self.parent.node.game()
+            self.parent.make_random_move()
+    
+    def mouse_button_down(self, event):
+        x, y = event.pos
+        file_index, rank_index = self.parent.pixel_to_square(x, y)
         square = chess.square(file_index, rank_index)
-        piece = board.piece_at(square)
-        if piece is None:
-            continue
-        piece_surface = get_piece_surface(piece.piece_type, piece.color)
-        x, y = get_coordinates(file_index, rank_index, orientation, BORDER)
-        window.blit(piece_surface, (x, y))
+        if self.active_square is None:
+            self.active_square = square
+        else:
+            move = chess.Move(self.active_square, square)
+            if self.active_square == square:
+                self.active_square = None
+                return
+            self.active_square = None
+            correct = False
+            variations = self.parent.node.variations
+            if not variations:
+                print("variation ended")
+                return
+            for child_node in variations:
+                if child_node.move == move:
+                    correct = True
+                    self.parent.node = child_node
+                    break
+            if correct:
+                print("correct")
+                self.parent.make_random_move()
+            else:
+                print("wrong")
+
+
+class Window:
+    def __init__(self):
+        with open('test.pgn') as f:
+            game = chess.pgn.read_game(f)
+        self.node = game
+        self.mode = PracticeMode(self)
+        self.orientation = chess.BLACK
+        self.surface = None
+    
+    def mainloop(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((SIZE, SIZE))
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    self.mode.key_down(event)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mode.mouse_button_down(event)
+            self.draw()
+            pygame.display.flip()
+        pygame.quit()
+    
+    def draw(self):
+        board = self.node.board()
+        # TODO: add arrows
+        board_svg = chess.svg.board(
+            board,
+            orientation=self.orientation,
+            # arrows=arrows,
+            size=SIZE
+        )
+        buffer = io.BytesIO(board_svg.encode())
+        board_surface = pygame.image.load(buffer)
+        self.surface.blit(board_surface, (0, 0))
+        for file_index, rank_index in itertools.product(range(8), repeat=2):
+            square = chess.square(file_index, rank_index)
+            piece = board.piece_at(square)
+            if piece is None:
+                continue
+            piece_surface = get_piece_surface(piece.piece_type, piece.color)
+            pos = self.square_to_pixel(file_index, rank_index)
+            self.surface.blit(piece_surface, pos)
+    
+    def flip_board(self):
+        self.orientation = not self.orientation
+    
+    def make_random_move(self):
+        if self.node.variations:
+            self.node = random.choice(self.node.variations)
+    
+    def square_to_pixel(self, file_index, rank_index):
+        if self.orientation:
+            rank_index = 7 - rank_index
+        else:
+            file_index = 7 - file_index
+        x = 15*BORDER + 45*file_index
+        y = 15*BORDER + 45*rank_index
+        return x, y
+    
+    def pixel_to_square(self, x, y):
+        file_index = (x - 15*BORDER) // 45
+        rank_index = (y - 15*BORDER) // 45
+        if self.orientation:
+            rank_index = 7 - rank_index
+        else:
+            file_index = 7 - file_index
+        return file_index, rank_index
 
 
 def main():
-    pygame.init()
-    window = pygame.display.set_mode((SIZE, SIZE))
-    
-    with open('test.pgn') as f:
-        game = chess.pgn.read_game(f)
-    
-    node = game
-    
-    orientation = chess.BLACK
-    
-    draw(node, orientation)
-    
-    active_square = None
-    
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_f:
-                    orientation = not orientation
-                    draw(node, orientation)
-                if event.key == pygame.K_LEFT:
-                    node = node.parent or node
-                    draw(node, orientation)
-                if event.key == pygame.K_RIGHT:
-                    if node.variations:
-                        node = random.choice(node.variations)
-                        draw(node, orientation)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                file, rank = get_square(x, y, orientation, BORDER)
-                square = chess.square(file, rank)
-                if active_square is None:
-                    active_square = square
-                else:
-                    move = chess.Move(active_square, square)
-                    active_square = None
-                    print(move)
-                    # TODO: check if move is in variations
-                    # TODO: play move
-        pygame.display.flip()
-    pygame.quit()
+    Window().mainloop()
 
 
 if __name__ == '__main__':
