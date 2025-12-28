d3784794b5b7c83a9ee045e646523ba5837e359a
Emil <emil.neumann1@web.de>
2025-12-29 00:35:36 +0100

    added treeview.py

---
+++ treeview.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+
+class TreeNode:
+    def __init__(self):
+        pass
+
+
+class TreeLine:
+    def __init__(self):
+        pass
+
+
+class TreeView:
+    def __init__(self, parent):
+        self.parent = parent
+    
+    def mouse_button_down(self, event):
+        pass
